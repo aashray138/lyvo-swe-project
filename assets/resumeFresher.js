2ce2a33bfe82793213bda1d39bad9aa373293717
aashray138 <55249252+aashray138@users.noreply.github.com>
2020-07-05 17:41:36 -0400

    Add files via upload

---
+++ assets/resumeFresher.js
@@ -0,0 +1,8 @@
+function loadNewPage() {
+
+    firstName_JS = document.getElementById("myForm").elements[0].value;
+
+    console.log(firstName_JS);
+
+    window.location = "resumeFresher.html";
+}
